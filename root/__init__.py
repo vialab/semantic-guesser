ccf2faeccb06fdad3d52810adfb6462a787ed31e
Rafael Veras <rafaveguim@gmail.com>
2012-02-14 09:25:14 -0500

    Starting with a small hardcode sample (from the manually curated dataset) tagged by a backoff chain trained with the brown corpus. Reasonable results. Better than penn treebank.

---
+++ root/__init__.py
@@ -0,0 +1,23 @@
+from tag_util import backoff_tagger
+from nltk.tag.sequential import DefaultTagger, BigramTagger, TrigramTagger
+from nltk.corpus import treebank
+from nltk.tag import UnigramTagger
+from nltk.corpus import brown
+
+
+phrases = [['holiday'],['I', 'love', 'erin'],['better', 'off'],['i', 'love', 'many'],
+           ['punk','rocker'], ['baby', 'love'], ['sex','me'], ['fuck', 'off'],
+           ['kill', 'yourself'], ['fuck', 'hoes']]
+
+#train_sents = treebank.tagged_sents()
+train_sents = brown.tagged_sents()
+
+backoff = DefaultTagger('NN')
+
+
+tagger  = backoff_tagger(train_sents, [UnigramTagger, BigramTagger, TrigramTagger], 
+                           backoff=backoff)
+
+for p in phrases :
+    print(tagger.tag(p))
+
---
+++ root/tag_util.py
@@ -0,0 +1,38 @@
+from nltk.tag import brill
+from nltk.probability import FreqDist, ConditionalFreqDist
+
+def backoff_tagger(train_sents, tagger_classes, backoff=None):
+    for cls in tagger_classes:
+        backoff = cls(train_sents, backoff=backoff)
+    
+    return backoff
+
+def word_tag_model(words, tagged_words, limit=200):
+    fd = FreqDist(words)
+    most_freq = fd.keys()[:limit]
+    cfd = ConditionalFreqDist(tagged_words)
+    return dict((word, cfd[word].max()) for word in most_freq)
+
+patterns = [
+    (r'^\d+$', 'CD'),
+    (r'.*ing$', 'VBG'), # gerunds, i.e. wondering
+    (r'.*ment$', 'NN'), # i.e. wonderment
+    (r'.*ful$', 'JJ') # i.e. wonderful
+]
+
+def train_brill_tagger(initial_tagger, train_sents, **kwargs):
+    sym_bounds = [(1,1), (2,2), (1,2), (1,3)]
+    asym_bounds = [(-1,-1), (1,1)]
+    
+    templates = [
+        brill.SymmetricProximateTokensTemplate(brill.ProximateTagsRule, *sym_bounds),
+        brill.SymmetricProximateTokensTemplate(brill.ProximateWordsRule, *sym_bounds),
+        brill.ProximateTokensTemplate(brill.ProximateTagsRule, *asym_bounds),
+        brill.ProximateTokensTemplate(brill.ProximateWordsRule, *asym_bounds)
+    ]
+    
+    trainer = brill.FastBrillTaggerTrainer(initial_tagger, templates, deterministic=True)
+    return trainer.train(train_sents, **kwargs)
+
+def unigram_feature_detector(tokens, index, history):
+    return {'word': tokens[index]}
