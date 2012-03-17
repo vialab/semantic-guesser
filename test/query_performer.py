6f8daebddf6229168b9a37c24d642d4ded450af5
Rafael Veras <rafaveguim@gmail.com>
2012-03-16 20:45:17 -0400

    Wrote a script to perform queries. It's way better than using the MySQL workbench because it uses streaming.

---
+++ test/query_performer.py
@@ -0,0 +1,31 @@
+'''
+Created on Mar 13, 2012
+
+@author: Rafa
+'''
+
+import MySQLdb.cursors
+import csv
+import sys
+
+def main():
+    conn = MySQLdb.connect(host="localhost", # your host, usually localhost
+                     user="root", # your username
+                     passwd="root", # your password
+                     db="passwords",
+                     cursorclass = MySQLdb.cursors.SSCursor)
+    query = "SELECT s.set_id, dict.dict_text, s.category, s.sent, s.pos FROM passwords.set_contains as s  " + \
+                            "left join dictionary as dict on dict.dict_id=s.dict_id;"
+    cursor = conn.cursor()
+    cursor.execute(query)
+    
+    nResults = 30000
+    csv_writer = csv.writer(open("result_set.csv","wb"), dialect='excel')
+    for i in range(nResults):
+        row = [ v for v in cursor.fetchone()]
+        row = [x if x is not None else "Null" for x in row]
+        csv_writer.writerow(row)
+    print "the end."
+    
+if __name__ == '__main__':
+    main()
