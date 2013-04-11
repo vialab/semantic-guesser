1c3eb76bb7126b1fe946c5e00e554fe69ac57ad5
Rafa <rafaveguim@gmail.com>
2013-04-11 11:47:28 -0400

    Improved CLI.

--- root/unevencut.py
+++ root/unevencut.py
@@ -56,16 +56,37 @@ def main(pos, cutter, cut_file, samplesize, tree_file, threshold):
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('pos', help='part-of-speech')
-    parser.add_argument('-f', '--file', help='complete path of the output file')
+    parser.add_argument('-f', '--file', help='directory path for the output file')
     parser.add_argument('-s', '--samplesize', type=int, help='if this argument is not '
                                                              'passed, runs over the entire database')
-    parser.add_argument('-t', '--tree', help='if a path is provided,'
+    parser.add_argument('-t', '--tree', help='if a directory path is provided,'
                                              ' the populated tree will be ouput to the informed file')
     parser.add_argument('-o', '--threshold', default=0, type=int, help='nodes with value <= threshold are removed')
     parser.add_argument('-a', '--algorithm', default='li_abe', choices=['li_abe', 'wagner'])
+    parser.add_argument('-w', '--weighting', default=50, type=int, help="weighting factor for Wagner's algorithm")
+    parser.add_argument('-c', '--commit', default=None, help="if the commit key is informed, the output"
+                                                             " file will feature it")
 
     args = parser.parse_args()
 
-    cutter = cut.li_abe if args.algorithm == 'li_abe' else 'wagner'
+    algorithm = args.algorithm
 
-    main(args.pos, cutter, args.file, args.samplesize, args.tree, args.threshold)
+    if algorithm == 'li_abe':
+        cutter = cut.li_abe
+    else:
+        cut.wagner.default_c = args.weighting
+        cutter = cut.wagner
+
+    # forming the cut file name
+    filename = "{}-{}".format(args.pos, args.algorithm)
+    if args.algorithm == 'wagner':
+        filename += '-{}'.format(args.weighting)
+    if args.samplesize:
+        filename += '-{}'.format(args.samplesize)
+    if args.commit:
+        filename += '-{}'.format(args.commit)
+
+    cut_file_path = "{}cut-{}.txt".format(args.file, filename)
+    tree_file_path = "{}tree-{}.json".format(args.tree, filename) if args.tree else None
+
+    main(args.pos, cutter, cut_file_path, args.samplesize, tree_file_path, args.threshold)
