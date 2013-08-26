99c91c9ad2bd88d4077a0e2a7a150eabaf4e2dc7
Rafa <rafaveguim@gmail.com>
2013-08-26 11:21:20 -0400

    Make file and a couple scripts.

---
+++ liabe_vs_wagner.sh
@@ -0,0 +1,11 @@
+python /home/rafa/Dev/workspace/pwd_classifier/root/unevencut.py n -a li_abe -o 0 -s 5000000 -f /home/rafa/Dev/workspace/pwd_classifier/results/cut/cut-nouns-li_abe-5mi-0-06141a7dc4.txt -t /home/rafa/Dev/workspace/pwd_classifier/results/cut/tree-nouns-li_abe-5mi-0-06141a7dc4.json
+
+python /home/rafa/Dev/workspace/pwd_classifier/root/unevencut.py n -a li_abe -o 0 -s 500000 -f /home/rafa/Dev/workspace/pwd_classifier/results/cut/cut-nouns-li_abe-500k-0-06141a7dc4.txt -t /home/rafa/Dev/workspace/pwd_classifier/results/cut/tree-nouns-li_abe-500k-0-06141a7dc4.json
+
+python /home/rafa/Dev/workspace/pwd_classifier/root/unevencut.py n -a li_abe -o 0 -s 100000 -f /home/rafa/Dev/workspace/pwd_classifier/results/cut/cut-nouns-li_abe-100k-0-06141a7dc4.txt -t /home/rafa/Dev/workspace/pwd_classifier/results/cut/tree-nouns-li_abe-100k-0-06141a7dc4.json
+
+python /home/rafa/Dev/workspace/pwd_classifier/root/unevencut.py n -a wagner -o 0 -s 5000000 -f /home/rafa/Dev/workspace/pwd_classifier/results/cut/cut-nouns-wagner-5mi-0-06141a7dc4.txt -t /home/rafa/Dev/workspace/pwd_classifier/results/cut/tree-nouns-wagner-5mi-0-06141a7dc4.json
+
+python /home/rafa/Dev/workspace/pwd_classifier/root/unevencut.py n -a wagner -o 0 -s 500000 -f /home/rafa/Dev/workspace/pwd_classifier/results/cut/cut-nouns-wagner-500k-0-06141a7dc4.txt -t /home/rafa/Dev/workspace/pwd_classifier/results/cut/tree-nouns-wagner-500k-0-06141a7dc4.json
+
+python /home/rafa/Dev/workspace/pwd_classifier/root/unevencut.py n -a wagner -o 0 -s 100000 -f /home/rafa/Dev/workspace/pwd_classifier/results/cut/cut-nouns-wagner-100k-0-06141a7dc4.txt -t /home/rafa/Dev/workspace/pwd_classifier/results/cut/tree-nouns-wagner-100k-0-06141a7dc4.json
---
+++ wagner_weighting_comparison.sh
@@ -0,0 +1,5 @@
+python /home/rafa/Dev/workspace/pwd_classifier/root/unevencut.py n -a wagner -w 1000 -o 0 -s 5000000 -c 1c3eb76 -f /home/rafa/Dev/workspace/pwd_classifier/results/cut/ -t /home/rafa/Dev/workspace/pwd_classifier/results/cut/
+
+python /home/rafa/Dev/workspace/pwd_classifier/root/unevencut.py n -a wagner -w 5000 -o 0 -s 5000000 -c 1c3eb76 -f /home/rafa/Dev/workspace/pwd_classifier/results/cut/ -t /home/rafa/Dev/workspace/pwd_classifier/results/cut/
+
+python /home/rafa/Dev/workspace/pwd_classifier/root/unevencut.py n -a wagner -w 10000 -o 0 -s 5000000 -c 1c3eb76 -f /home/rafa/Dev/workspace/pwd_classifier/results/cut/ -t /home/rafa/Dev/workspace/pwd_classifier/results/cut/
