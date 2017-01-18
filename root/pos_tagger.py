b1f22d78b7fc1993fdc82cae3e79f8a3f4184b79
Rafael <rafaveguim@gmail.com>
2017-01-18 10:56:00 -0500

    [fix] error in the calculation of progress in output

--- root/pos_tagger.py
+++ root/pos_tagger.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import database 
+import database
 from nltk.tag.sequential import DefaultTagger, BigramTagger, TrigramTagger,\
 SequentialBackoffTagger
 from nltk.probability import FreqDist
@@ -10,14 +10,15 @@ import sys
 import traceback
 from timer import Timer
 import argparse
+import util
 
 
 class BackoffTagger(SequentialBackoffTagger):
-    
+
     def __init__(self, *args, **kwargs):
         SequentialBackoffTagger.__init__(self, *args, **kwargs)
         self.dist = FreqDist()
-        
+
 #       train_sents = brown.tagged_sents()
         train_sents = pickle.load(open("pickles/brown_clawstags.pickle"))
         # make sure all tuples are in the required format: (TAG, word)
@@ -29,66 +30,64 @@ class BackoffTagger(SequentialBackoffTagger):
         coca_tagger    = COCATagger(names_tagger)
         bigram_tagger  = BigramTagger(train_sents, backoff=coca_tagger)
         trigram_tagger = TrigramTagger(train_sents, backoff=bigram_tagger)
-        
+
         # doesn't include self cause it's a dumb tagger (would always return None)
-        self._taggers = trigram_tagger._taggers 
+        self._taggers = trigram_tagger._taggers
 
     def tag_one(self, tokens, index, history):
         tag = None
         for tagger in self._taggers:
             tag = tagger.choose_tag(tokens, index, history)
-            if tag is not None:  
+            if tag is not None:
                 #self.dist.inc(tagger.__class__.__name__)
                 self.dist[tagger.__class__.__name__] += 1
 #                  print tokens[index], history, tagger.__class__.__name__, tag
                 break
-        return tag 
+        return tag
 
 
 #TODO: Isolate the POS tagging code in this function
 def POStag(password, tagger):
     """ Part-of-speech tag a single password.
     Sets the POS attribute of the Fragments.
-    password: a list of Fragment objects  
+    password: a list of Fragment objects
     """
     pass
-    
-
-
 
 
 def main(db, dryrun, stats, verbose):
     """ Tags the dataset by POS and sentiment at
-        the same time """    
-    
-    with Timer("Backoff tagger load"):    
+        the same time """
+
+    with Timer("Backoff tagger load"):
         pos_tagger = BackoffTagger()
-    
+
     counter = 0
-    
+    output_interval = round(db.savebuffer_size/5)
+
     with Timer("POS tagging"):
-        total = db.sets_size
-        
+        total = db.pwset_size
+
         print "Connected to database, tagging..."
-        
-        lastpw = None        
-        
+
+        lastpw = None
+
         while db.hasNext():
             pwd = db.nextPwd()  # list of Fragment
             pwd_str = pwd[0].password
 
             counter += 1
-                
+
             # filters segments that are not dictionary words
             pwd = [f for f in pwd if f.dictset_id <= 90]
-            
+
             # only recalculate POS if this password is diff than previous
             if pwd_str != lastpw:
-            
+
                 # extracts to a list of strings and tags them
                 pos_tagged = pos_tagger.tag([f.word for f in pwd])
 
-            
+
             for i, f in enumerate(pwd):
                 pos = pos_tagged[i][1]  # Brown pos tag
                 f.pos = pos
@@ -96,20 +95,22 @@ def main(db, dryrun, stats, verbose):
                     db.save(f, True)
                 if verbose:
                     print "{}\t{}\t{}".format(f.password, f.word, f.pos)
-            
+
             lastpw = pwd_str
-    
-            if counter % 100000 == 0:
-                print "{} passwords processed. {}% completed..."\
+
+            if counter % output_interval == 0:
+                # util.printprogress(counter/output_interval, total/output_interval)
+                print "{} passwords processed. {:.2f}% completed..."\
                 .format(counter, (float(counter)/total)*100)
-        
+                # print "{} passwords processed.".format(counter)
+
         db.finish()
-        
+
         if stats:
             print "\nFrequency distribution of results by tagger\n"
             for k, v in pos_tagger.dist.items():
                 print "{}\t{}".format(k, v)
-    
+
 
 def options():
     parser = argparse.ArgumentParser()
@@ -125,13 +126,13 @@ def options():
         help="output the pos tags of each password")
 
     return parser.parse_args()
- 
- 
+
+
 if __name__ == "__main__":
     opts = options()
 
     try:
-        db = database.PwdDb(opts.password_set, sample=opts.sample, save_cachesize=500000)
+        db = database.PwdDb(opts.password_set, sample=opts.sample, save_cachesize=100000)
         main(db, opts.dryrun, opts.stats, opts.verbose)
     except:
         e = sys.exc_info()[0]
@@ -149,7 +150,3 @@ if __name__ == "__main__":
 #     print nltk.pos_tag(['all','the', 'cake'])
 #     print nltk.pos_tag(['all', 'alone'])
 #     print nltk.pos_tag(['to', 'lose', "one's", 'all'])
-
-
-
-
