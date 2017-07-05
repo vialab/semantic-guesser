d2f473d60b24161936fdc351fde35fbf0d11ea4d
Rafa <rafaveguim@gmail.com>
2017-07-05 14:41:51 -0400

    refactoring

--- root/test/estimation.py
+++ root/test/estimation.py
@@ -21,8 +21,44 @@ import root.util as util
 from collections     import defaultdict
 from root.pos_tagger import BackoffTagger
 from root.estimator  import LaplaceEstimator, MleEstimator, FreqDist, noun_vocab, verb_vocab
+import root.sampler as sampler
+import pandas as pd
+import numpy  as np
 
-def main():
+
+def n_trials(n, pwset_id, samplesize):
+    """ Records the results of N trials of this test."""
+    df = None
+    for i in range(1, n+1):
+        sampler.sample_passwords(pwset_id, samplesize) # resample
+        lap, emp, pmf_lap, mle_binned = test()
+
+        trial = pd.DataFrame({
+            'laplace'  : util.values_sorted_by_key(lap),
+            'empirical': util.values_sorted_by_key(emp),
+            'pmf_lap'  : util.values_sorted_by_key(pmf_lap)
+        }, index = sorted(lap.keys())).unstack().reset_index()
+        trial['trial'] = i + 1
+
+        trial.columns  = ['estimator', 'r', 'estimate', 'trial']
+        if df is None:
+            df = trial
+        else:
+            df = df.append(trial, ignore_index=True)
+
+    agg = df.groupby(['estimator', 'r']) \
+            .aggregate({'estimate': [np.mean, np.std]})
+    agg = agg.unstack(level=0)
+
+    mle_count = map(lambda d: len(d), util.values_sorted_by_key(mle_binned))
+    agg = pd.concat([agg, pd.DataFrame({'mle_count': mle_count}, 
+                          index=sorted(mle_binned))], axis=1)
+    agg['mle_count'].fillna(0, inplace=True)
+    
+    agg.to_csv('root/test/estimation.out')
+
+
+def test():
     tagger = BackoffTagger()
     vocabulary = noun_vocab(tagger)
     vocabulary.update(verb_vocab(tagger))
@@ -49,14 +85,14 @@ def main():
                     lap.inc((s.word, s.pos))
                     mle.inc((s.word, s.pos))
                 else:
-                   test_dist.inc((s.word, s.pos))
+                    test_dist.inc((s.word, s.pos))
         i += 1
         if i == interval:
             i = 0
     	    k += 1
-            util.printprogress(k, n)
+            util.printprogress(k, n, barLength=50)
 
-    util.printprogress(k, n)
+    # util.printprogress(k, n, barLength=50)
     db.finish()
 
     # Now we want to see how well the estimators predict the frequencies
@@ -68,12 +104,13 @@ def main():
     # In the same way, how do these compare with the avg. freq. predicted by the
     # Laplace estimator (f_lap).
 
-	
+
     mle_binned  = mle.group_by_freq() # estimated frequency of frequencies in test data
     freqs       = mle_binned.keys()
     f_empirical = FreqDist(freqs) # empirical avg. frequency of frequencies in test data
     lap_binned  = lap.group_by_freq() # estimated avg. frequency of frequencies in test data (Laplace method)
-    
+    pmf_lap     = lap.pmf_by_freq()
+
     f_lap = defaultdict(lambda:0)
     # Iterate over vocabulary
     # 1. tally observed word counts aggregated by count in training data (f_empirical)
@@ -88,26 +125,16 @@ def main():
         f_lap[f] *= float(test_dist.total) / len(lemma_list)
         f_empirical[f] = float(f_empirical[f])/len(lemma_list)
 
-    print "r\tf_empirical\tf_lap\n"
-    for r, f_emp in f_empirical.items():
-        print "{}\t{}\t{}".format(r, f_emp, f_lap[r])
+    return [f_lap, f_empirical, pmf_lap, mle_binned]
+#    print "r\tf_empirical\tf_lap\n"
+#    for r, f_emp in f_empirical.items():
+#        print "{}\t{}\t{}".format(r, f_emp, f_lap[r])
 
-        
-
-	
-
-	# ---------- test data ------------------
-#	test_dist = FreqDist([])
-#	db = PwdDb(1, test=1)
-#	while db.hasNext():
-#		segments = db.nextPwd()
-#		for s in segments:
-#			if s.dictset_id
-    
 
 
 if __name__ == "__main__":
-    main()
+#    main()
+    n_trials(10, 1, 16292238)
 
 # TODO:
 # build avg. frequencies of frequencies for training data
